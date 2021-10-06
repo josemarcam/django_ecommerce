ed6404099633095d3578bb754b952d5523d98e1b
Jose Martins <josemartins.camargo@gmail.com>
2021-10-06 20:12:31 -0300

    setup settings

--- pystore/settings.py
+++ pystore/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'pystore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pystore_dev',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': 8889,
     }
 }
 
